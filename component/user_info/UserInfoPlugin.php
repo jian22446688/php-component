978c8927872bf39513cfdb7c53569b4f073e81c1
798171920@qq.com <798171920@qq.com>
2018-11-26 17:39:54 +0800

    添加自定义验证码
    添加自定义验证码

--- component/user_info/UserInfoPlugin.php
+++ component/user_info/UserInfoPlugin.php
@@ -26,6 +26,11 @@ class UserInfoPlugin extends Plugin {
 
     public $hasAdmin = 1;//插件是否有后台管理界面
 
+    public $types = ['123456789',
+        'ABCDEFGHIJKLMNPQRSTUVWXYZabcdefghjkmnopqrstuvwxyz',
+        '123456789ABCDEFGHIJKLMNPQRSTUVWXYZabcdefghjkmnopqrstuvwxyz'
+    ];
+
     public function install() {
         // TODO: Implement install() method.
         // 安装插件时初始化创建 数据库
@@ -42,4 +47,32 @@ class UserInfoPlugin extends Plugin {
     public function uninstall() {
         return true;
     }
+
+    // 加入 修改验证码
+    public function captchaImage($param) {
+        $captcha = $this->getConfig();
+        if ($captcha['type'] == 3) {
+            $codeSet = $this->types[2];
+            $captcha['useZh'] = 1;
+        } else {
+            $codeSet = $this->types[$captcha['type']];
+        }
+        $captcha['codeSet'] = $codeSet;
+        unset($captcha['type']);
+        $bg = explode(',', $captcha['bg']);
+        array_walk($bg, 'intval');
+        if (count($bg) > 2 && $bg[0] < 256 && $bg[1] < 256 && $bg[2] < 256) {
+            $captcha['bg'] = $bg;
+        } else {
+            $captcha['bg'] = '';
+        }
+        foreach ($captcha as $k => &$v) {
+            //接收前台长宽
+            if (in_array($k,['imageW','imageH'] )&&isset($param[$k]) && $param[$k] > 0) {
+                $v = $param[$k];
+            }
+        }
+        config('captcha', $captcha);
+    }
+
 }
